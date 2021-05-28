240149a3f95a166ba0ac790bc240970da8fb0d26
Zamzawi <idhamzamzawi@gmail.com>
2021-05-28 17:27:09 +0700

    Chat room created

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -9,7 +9,10 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Container
+                    <message-container :messages="messages" />
+                    <input-message 
+                        :room="currentRoom"
+                        v-on:messagesent="getMessages()" />
                 </div>
             </div>
         </div>
@@ -17,11 +20,50 @@
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
+import AppLayout from '@/Layouts/AppLayout'
+import InputMessage from './inputMessage.vue'
+import MessageContainer from './messageContainer.vue'
 
-    export default {
-        components: {
-            AppLayout,
+export default {
+    components: {
+        AppLayout,
+        MessageContainer,
+        InputMessage
+    },
+    data: function () {
+        return {
+            chatRooms: [],
+            currentRoom: [],
+            messages: []
+        }
+    },
+    methods: {
+        getRooms() {
+            axios.get('/chat/rooms')
+            .then( response => {
+                this.chatRooms = response.data;
+                this.setRoom( response.data[0] );
+            })
+            .catch( error => {
+                console.log( error );
+            })
         },
+        setRoom( room ) {
+            this.currentRoom = room;
+            this.getMessages();
+        },
+        getMessages() {
+            axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+            .then( response => {
+                this.messages = response.data;
+            })
+            .catch( error => {
+                console.log( error );
+            })
+        }
+    },
+    created() {
+        this.getRooms();
     }
+}
 </script>
