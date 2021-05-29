2f81c64a62037192539582cb9ac919a6fa7b93e4
Zamzawi <idhamzamzawi@gmail.com>
2021-05-29 21:02:33 +0700

    Finish front-end

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -43,7 +43,28 @@ export default {
             messages: []
         }
     },
+    watch: {
+        currentRoom( val, oldVal ) {
+            if( oldVal.id ) {
+                this.disconnect( oldVal );
+            }
+            this.connect();
+        }
+    },
     methods: {
+        connect() {
+            if( this.currentRoom.id ) {
+                let vm = this;
+                this.getMessages();
+                window.Echo.private("chat." + this.currentRoom.id )
+                .listen('NewChatMessage', e => {
+                    vm.getMessages();
+                })
+            }
+        },
+        disconnect( room ) {
+            window.Echo.leave("chat." + room.id );
+        },
         getRooms() {
             axios.get('/chat/rooms')
             .then( response => {
@@ -56,7 +77,6 @@ export default {
         },
         setRoom( room ) {
             this.currentRoom = room;
-            this.getMessages();
         },
         getMessages() {
             axios.get('/chat/room/' + this.currentRoom.id + '/messages')
